fd3c26a0c81dc84b2e5119d65501c1abe1685e81
Szymon Andrzejewski <monandszy@gmail.com>
2023-12-02 17:20:56 +0100

    Added Day2

---
+++ src/main/java/day1/Day1.java
@@ -0,0 +1,97 @@
+package day1;
+
+import utils.DayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Day1 {
+    public static void main(String[] args) {
+        partOne();
+        partTwo();
+    }
+
+    private static void partOne() {
+        DayUtils dayUtils = new DayUtils(1, 1, DayUtils.FetchOption.RE_FETCH);
+        Stream<String> streamInput = dayUtils.getListInput().stream();
+        dayUtils.startTimer();
+        var i = streamInput
+                .map(String::chars)
+                .map(e -> e.map(Character::getNumericValue))
+                .map(intStream -> intStream.filter(character -> character >= 1 && character <= 9))
+                .map(IntStream::toArray)
+                .filter(e -> e.length > 0)
+                .map(array -> String.valueOf(array[0]) + array[array.length - 1])
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .orElseThrow();
+        dayUtils.endTimer();
+        dayUtils.printAnswer(i);
+    }
+
+    private static void partTwo() {
+        DayUtils dayUtils = new DayUtils(1, 2);
+        Stream<String> streamInput = dayUtils.getListInput().stream();
+        dayUtils.startTimer();
+        var i = streamInput
+                .map(Day1::getNumbers)
+                .map(List::toArray)
+                .map(array -> String.valueOf(array[0]) + array[array.length - 1])
+                .map(Integer::parseInt)
+                .reduce(Integer::sum)
+                .orElseThrow();
+        dayUtils.endTimer();
+        dayUtils.printAnswer(i);
+    }
+
+
+    private static List<Integer> getNumbers(String line) {
+        List<Integer> numberList = new ArrayList<>();
+        Map<Integer, String> occurrences = new TreeMap<>();
+        int formIndex = 0;
+        for (var entry : stringToInteger.entrySet()) {
+            while (line.substring(formIndex).contains(entry.getKey())) {
+                formIndex = line.indexOf(entry.getKey(), formIndex);
+                occurrences.put(formIndex, entry.getKey());
+                formIndex++;
+            }
+            formIndex = 0;
+        }
+        for (var occurrence : occurrences.entrySet()) {
+            numberList.add(stringToInteger.get(occurrence.getValue()));
+        }
+        return numberList;
+    }
+
+    private static final Map<String, Integer> stringToInteger = new TreeMap<>();
+
+    static {
+        stringToInteger.putAll(Map.of(
+                "one", 1,
+                "two", 2,
+                "three", 3,
+                "four", 4,
+                "five", 5,
+                "six", 6,
+                "seven", 7,
+                "eight", 8,
+                "nine", 9
+
+        ));
+        stringToInteger.putAll(Map.of(
+                "1", 1,
+                "2", 2,
+                "3", 3,
+                "4", 4,
+                "5", 5,
+                "6", 6,
+                "7", 7,
+                "8", 8,
+                "9", 9
+        ));
+    }
+}
---
+++ src/main/java/day2/Day2.java
@@ -0,0 +1,73 @@
+package day2;
+
+import utils.DayUtils;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        partOne();
+        partTwo();
+    }
+
+    private static void partOne() {
+        DayUtils dayUtils = new DayUtils(2, 1);
+        dayUtils.startTimer();
+        Integer i = dayUtils.getListInput().stream()
+                .map(e -> e.split(";"))
+                .map(Day2::extractSummarizedGame)
+                .filter(round -> round.red <= 12 && round.green <= 13 && round.blue <= 14)
+                .map(game -> game.gameNumber)
+                .reduce(Integer::sum)
+                .orElseThrow();
+        dayUtils.endTimer();
+        dayUtils.printAnswer(i);
+
+    }
+
+    private static void partTwo() {
+        DayUtils dayUtils = new DayUtils(2, 2);
+        dayUtils.startTimer();
+        Integer i = dayUtils.getListInput().stream()
+                .map(e -> e.split(";"))
+                .map(Day2::extractSummarizedGame)
+                .map(e -> e.red * e.green * e.blue)
+                .reduce(Integer::sum)
+                .orElseThrow();
+        dayUtils.endTimer();
+        dayUtils.printAnswer(i);
+
+    }
+
+    private static SummarizedGame extractSummarizedGame(String[] strings) {
+        int gameNumber = extractGameNumber(strings);
+        List<String[]> rounds = Arrays.stream(strings).map(e -> e.split(",")).toList();
+        Integer red = getMaxSummarizedColorValue(rounds, "red");
+        Integer green = getMaxSummarizedColorValue(rounds, "green");
+        Integer blue = getMaxSummarizedColorValue(rounds, "blue");
+        return new SummarizedGame(gameNumber, red, green, blue);
+    }
+
+    private static Integer getMaxSummarizedColorValue(List<String[]> rounds, String color) {
+        return rounds.stream().map(array -> Arrays.stream(array)
+                .filter(colorValue -> colorValue.contains(color))
+                .map(colorValue -> Integer.parseInt(colorValue.substring(0, colorValue.lastIndexOf(' ')).strip()))
+                .findFirst().orElse(0)).max(Comparator.comparingInt(e -> e)).orElseThrow();
+    }
+
+    private static int extractGameNumber(String[] games) {
+        int gameNumber = Integer.parseInt(games[0].substring(
+                games[0].indexOf("Game") + 5, games[0].indexOf(':')));
+        games[0] = games[0].split(":")[1]; // gets rid of gameNumber
+        return gameNumber;
+    }
+
+    public record GameRound(int red, int green, int blue) {
+    }
+
+    public record SummarizedGame(int gameNumber, int red, int green, int blue) {
+    }
+}
