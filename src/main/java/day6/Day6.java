f9b1ef4168a7cc1392889083a3b42bba579cb7f4
Szymon Andrzejewski <monandszy@gmail.com>
2023-12-08 00:03:06 +0100

    Day 6 confusion

--- src/main/java/day6/Day6.java
+++ src/main/java/day6/Day6.java
@@ -6,7 +6,6 @@ import utils.RegexPatterns;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
@@ -44,15 +43,32 @@ public class Day6 {
         List<Integer> waysCount = new ArrayList<>();
         for (Race race : races) {
             findWays(race, waysCount);
+//            solveHoldTime(race);
         }
         int multiply = waysCount.stream().reduce((a, b) -> a * b).orElseThrow();
         dayUtils.endTimer();
         dayUtils.printAnswer(multiply);
     }
 
+    private static long solveHoldTime(Race race) {
+        long a = -1;
+        long b = race.holdTime();
+        long c = -race.distance();
+        double delta = Math.sqrt(b * b - 4 * a * c);
+        double sol1 = (b + delta) / 2; // why not multiply by a ????!?!
+        double sol2 = (b - delta) / 2; // I'm confused
+        System.out.println(sol1 + " " + sol2);
+        double range = sol1 - sol2;
+        System.out.println(calculateDistance(race, sol1));
+        System.out.println(calculateDistance(race, sol2));
+        return (long) (
+                sol1 - Math.ceil(sol2) + 1 - 2 * ((delta % 1) == 0 ? 1 : 0)
+        );
+    }
+
     private static void findWays(Race race, List<Integer> waysCount) {
         Set<Integer> distanceSet = new HashSet<>();
-        for (int timeWaited = 0; timeWaited < race.time(); timeWaited++) {
+        for (int timeWaited = 0; timeWaited < race.holdTime(); timeWaited++) {
             double distance = calculateDistance(race, timeWaited);
             if (distance > race.distance()) {
                 distanceSet.add(timeWaited);
@@ -62,24 +78,8 @@ public class Day6 {
     }
 
     private static double calculateDistance(Race race, double timeWaited) {
-        return race.time() * timeWaited - Math.pow(timeWaited, 2);
+        return race.holdTime() * timeWaited - Math.pow(timeWaited, 2);
     }
-
-
-//    private static int calculateTimeWaited(Race race) {
-//        int a = -1;
-//        int b = race.time();
-//        int c = -race.distance();
-//        double delta = Math.pow(b, 2) - 4 * a * c;
-//        System.out.println("delta: " + delta);
-//        double sol1 = ((b + Math.sqrt(delta)) / (2 * a));
-//        double sol2 = ((b - Math.sqrt(delta)) / (2 * a));
-//        System.out.println(sol1 + " " + sol2);
-//        calculateDistance(race, sol1);
-//        calculateDistance(race, sol2);
-//        return 0;
-//    }
-
 
     private static List<Race> getRaces(List<String> input) {
         List<Integer> times = getNumbers(input.get(0));
@@ -96,6 +96,6 @@ public class Day6 {
         return matcher.results().map(MatchResult::group).map(Integer::parseInt).toList();
     }
 
-    public record Race(long time, long distance) {
+    public record Race(long holdTime, long distance) {
     }
 }
